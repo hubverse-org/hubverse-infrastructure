213ac3c87fde2ebf26882e1498650d264962ddf9
Becky Sweger <rsweger@umass.edu>
2024-04-05 13:40:13 -0400

    Add ruff code formatter to the project
    Add ruff, a corresponding config file, and the formatting
    changes that it suggested

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -4,13 +4,14 @@ import yaml
 
 from hubs.hub_setup import set_up_hub
 
+
 def get_hubs() -> list[dict]:
-    '''Get the list of cloud-enabled hubs.'''
-    with open('hubs/hubs.yaml', 'r') as file:
-        hubs = yaml.safe_load(file).get('hubs')
+    """Get the list of cloud-enabled hubs."""
+    with open("hubs/hubs.yaml", "r") as file:
+        hubs = yaml.safe_load(file).get("hubs")
     return hubs
+
 
 hub_list = get_hubs()
 for hub in hub_list:
     set_up_hub(hub)
-
--- pulumi/hubs/hub_setup.py
+++ pulumi/hubs/hub_setup.py
@@ -3,13 +3,14 @@
 from hubs.s3 import create_s3_infrastructure
 from hubs.iam import create_iam_infrastructure
 
-def set_up_hub(hub_info:dict):
-    '''
+
+def set_up_hub(hub_info: dict):
+    """
     Create all AWS instrastructure needed for a Hubverse hub.
     For simplicity, this demo uses the hub name as the bucket name,
     though the new cloud section of a hub's admin.json config allows
     a different bucket name.
-    '''
+    """
 
     create_s3_infrastructure(hub_info)
     create_iam_infrastructure(hub_info)
