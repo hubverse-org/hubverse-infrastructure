80b324a82c1818a4e10740e41d9843f725d57cc8
Becky Sweger <rsweger@umass.edu>
2024-04-18 11:46:56 -0400

    Move permissions components of lambda to their own function
    This changeset also creates an IAM policy to allows writes to CloudWatch
    logs and attaches that policy to the IAM role assumed by our
    hubverse-transform lambda function.

--- src/hubverse_infrastructure/shared/hubverse_transforms.py
+++ src/hubverse_infrastructure/shared/hubverse_transforms.py
@@ -11,7 +11,9 @@ def create_bucket(bucket_name: str) -> aws.s3.Bucket:
 
     hubverse_asset_bucket = aws.s3.Bucket(bucket_name, bucket=bucket_name, tags={"hub": "hubverse"})
 
-    # Create an S3 policy that will the AWS Lambda service to read from the bucket
+    # Create an S3 policy that allows the AWS Lambda service to read from the bucket
+    # (this is a permissive read policy because it applies to any lambda function, but the Hubverse
+    # deals in open source data and code, so we can leave keep it open for simplicity)
     s3_policy_document = aws.iam.get_policy_document(
         statements=[
             aws.iam.GetPolicyDocumentStatementArgs(
@@ -52,11 +54,38 @@ def create_bucket(bucket_name: str) -> aws.s3.Bucket:
     return hubverse_asset_bucket
 
 
-def create_transform_lambda(lambda_name: str, package_location: CloudPath) -> aws.lambda_.Function:
-    """
-    Create the scaffolding for the Lambda function that transforms a model-output file.
-    """
+def create_cloudwatch_write_policy(policy_name: str):
+    # Create a policy that allows write access to AWS CloudWatch logs
+    # (we'll need to attach this to the Lambda execution role)
+    cloudwatch_write_policy_document = aws.iam.get_policy_document(
+        statements=[
+            aws.iam.GetPolicyDocumentStatementArgs(
+                actions=[
+                    "logs:PutLogEvents",
+                    "logs:CreateLogGroup",
+                    "logs:CreateLogStream",
+                ],
+                resources=["arn:aws:logs:*:*:*"],
+            ),
+        ]
+    )
 
+    cloudwatch_write_policy_name = policy_name
+    cloudwatch_write_policy = aws.iam.Policy(
+        name=cloudwatch_write_policy_name,
+        resource_name=cloudwatch_write_policy_name,
+        description="Policy that allows writing to AWS CloudWatch logs",
+        policy=cloudwatch_write_policy_document.json,
+        tags={"hub": "hubverse"},
+    )
+
+    return cloudwatch_write_policy
+
+
+def create_lambda_execution_permissions(lambda_name: str) -> aws.iam.Role:
+    """Create IAM role that the hubverse-transform lambda will assume and attach the necessary permissions."""
+
+    # Create the role used by the lambda function
     lambda_role_document = aws.iam.get_policy_document(
         statements=[
             aws.iam.GetPolicyDocumentStatementArgs(
@@ -79,6 +108,22 @@ def create_transform_lambda(lambda_name: str, package_location: CloudPath) -> aw
         assume_role_policy=lambda_role_document.json,
         tags={"hub": "hubverse"},
     )
+
+    # Create a policy that allows writes to AWS CloudWatch logs and attach it to the lambda role
+    cloudwatch_write_policy = create_cloudwatch_write_policy("hubverse-cloudwatch-write-policy")
+    aws.iam.RolePolicyAttachment(
+        resource_name=f"{lambda_name}-cloudwatch-policy", role=lambda_role.name, policy_arn=cloudwatch_write_policy.id
+    )
+
+    return lambda_role
+
+
+def create_transform_lambda(
+    lambda_name: str, package_location: CloudPath, lambda_role: aws.iam.Role
+) -> aws.lambda_.Function:
+    """
+    Create the scaffolding for the Lambda function that transforms a model-output file.
+    """
 
     s3_bucket = package_location.drive
     s3_key = package_location.key
@@ -108,4 +153,5 @@ def create_transform_infrastructure():
     lambda_package_path = CloudPath(lambda_package_location)
 
     create_bucket(bucket_name)
-    create_transform_lambda(lambda_name, package_location=lambda_package_path)
+    lambda_role = create_lambda_execution_permissions(lambda_name)
+    create_transform_lambda(lambda_name, lambda_package_path, lambda_role)
