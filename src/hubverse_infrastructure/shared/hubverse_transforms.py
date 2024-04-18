a71c161c2f95f3598723e98e67ec380776e0201d
Becky Sweger <rsweger@umass.edu>
2024-04-18 17:18:02 -0400

    Create a placeholder lambda package on S3
    Annoyingly, the "create lambda" function will fail if it's pointing
    to a lambda code package that doesn't yet exist on S3. It creates a
    chicken-and-egg problem for us, since we haven't deployed the transform
    function's code to S3 yet. Might be overkill, but this changeset
    creates a placeholder .zip to use as lambda code package until
    we have the official deployment pipeline up and running (in the
    hubverse-transform repo)

--- src/hubverse_infrastructure/shared/hubverse_transforms.py
+++ src/hubverse_infrastructure/shared/hubverse_transforms.py
@@ -1,42 +1,49 @@
 """Create the AWS infrastructure needed to run Hubverse model-output transformations as Lambda functions."""
+import io
+from zipfile import ZipFile
+
+import boto3
+import pulumi
 import pulumi_aws as aws
+from botocore.exceptions import ClientError
 from cloudpathlib import CloudPath
 from pulumi import ResourceOptions  # type: ignore
 
 
-def create_bucket(bucket_name: str) -> aws.s3.Bucket:
+def create_bucket(bucket_name: str) -> aws.s3.BucketV2:
     """
     Create the S3 bucket used to store shared Hubverse assets and give the AWS Lambda service read access to it.
     """
 
-    hubverse_asset_bucket = aws.s3.Bucket(bucket_name, bucket=bucket_name, tags={"hub": "hubverse"})
+    hubverse_asset_bucket = aws.s3.BucketV2(bucket_name, bucket=bucket_name, tags={"hub": "hubverse"})
 
     # Create an S3 policy that allows the AWS Lambda service to read from the bucket
     # (this is a permissive read policy because it applies to any lambda function, but the Hubverse
-    # deals in open source data and code, so we can leave keep it open for simplicity)
+    # deals in open source data and code, so we can leave keep it open for simplicity).
     s3_policy_document = aws.iam.get_policy_document(
         statements=[
             aws.iam.GetPolicyDocumentStatementArgs(
-                sid="LambdaReadBucket",
+                sid="ReadAssetBucket",
                 actions=[
                     "s3:GetObject",
+                    "s3:GetObjectAcl",
                 ],
                 principals=[
                     aws.iam.GetPolicyDocumentStatementPrincipalArgs(
                         type="Service", identifiers=["lambda.amazonaws.com"]
-                    )
+                    ),
                 ],
                 resources=[f"arn:aws:s3:::{bucket_name}/*"],
             ),
             aws.iam.GetPolicyDocumentStatementArgs(
-                sid="LambdaListBucket",
+                sid="ListAssetBucket",
                 actions=[
                     "s3:ListBucket",
                 ],
                 principals=[
                     aws.iam.GetPolicyDocumentStatementPrincipalArgs(
                         type="Service", identifiers=["lambda.amazonaws.com"]
-                    )
+                    ),
                 ],
                 resources=[f"arn:aws:s3:::{bucket_name}"],
             ),
@@ -132,7 +139,7 @@ def create_lambda_execution_permissions(lambda_name: str) -> aws.iam.Role:
 
 
 def create_transform_lambda(
-    lambda_name: str, package_location: CloudPath, lambda_role: aws.iam.Role
+    lambda_name: str, package_location: CloudPath, lambda_role: aws.iam.Role, hubverse_asset_bucket: aws.s3.BucketV2
 ) -> aws.lambda_.Function:
     """
     Create the scaffolding for the Lambda function that transforms a model-output file.
@@ -140,6 +147,11 @@ def create_transform_lambda(
 
     s3_bucket = package_location.drive
     s3_key = package_location.key
+
+    # Using arn.apply below ensures that the create_lambda_package_placeholder doesn't run
+    # until the hubverse_asset_bucket exists (because a bucket's arn isn't available until
+    # the bucket has been physically created on AWS).
+    hubverse_asset_bucket.arn.apply(lambda _: create_lambda_package_placeholder(s3_bucket, s3_key))
 
     transform_lambda = aws.lambda_.Function(
         name=lambda_name,
@@ -153,18 +165,53 @@ def create_transform_lambda(
         s3_key=s3_key,
         tags={"hub": "hubverse"},
         timeout=210,
+        opts=ResourceOptions(depends_on=[hubverse_asset_bucket]),
     )
 
     return transform_lambda
 
 
+def create_lambda_package_placeholder(s3_bucket: str, s3_key: str):
+    """Create a lambda package placeholder in S3 if necessary."""
+
+    # if we're in a dry run (such as a Pulumi preview), skip this
+    if pulumi.runtime.is_dry_run():
+        return
+
+    # This function creates a "scaffolding" lambda, which is designed to run packaged function code
+    # stored in an S3 bucket. We don't want Pulumi to manage the lambda's actual code because
+    # we don't want the hubverse transform functionality tightly coupled to the Hubverse-managed
+    # AWS infrastructure. However, if the code package (which is deployed by the hubverse-transform
+    # repo) doesn't exist yet, we need to create a placeholder zip file. It's a chicken-and-egg problem
+    # that should only occur until the hubverse-transform deployment pipeline is up and running)
+
+    s3 = boto3.resource("s3")
+    lambda_package = s3.Object(s3_bucket, s3_key)
+    try:
+        lambda_package.get()
+    except ClientError as e:
+        if e.response.get("Error", {}).get("Code") == "NoSuchKey":
+            placeholder_zip = io.BytesIO()
+            zip_file = ZipFile(placeholder_zip, "w")
+            zip_file.writestr("placeholder", b"lambda function placeholder")
+            zip_file.close()
+            placeholder_zip.seek(0)
+            lambda_package.put(Body=placeholder_zip)
+        else:
+            raise Exception(f"Boto error when checking for existing lambda package: {s3_bucket}/{s3_key}") from e
+    except Exception as e:
+        raise Exception(f"Error when checking for existing lambda package: {s3_bucket}/{s3_key}") from e
+
+    return lambda_package
+
+
 def create_transform_infrastructure():
     bucket_name = "hubverse-assets"
     lambda_name = "hubverse-transform-model-output"
-    lambda_package_location = "s3://hubverse-assets/lambda/hubverse-transform.zip"
+    lambda_package_location = "s3://hubverse-assets/lambda/hubverse-transform-model-output.zip"
 
     lambda_package_path = CloudPath(lambda_package_location)
 
-    create_bucket(bucket_name)
+    bucket = create_bucket(bucket_name)
     lambda_role = create_lambda_execution_permissions(lambda_name)
-    create_transform_lambda(lambda_name, lambda_package_path, lambda_role)
+    create_transform_lambda(lambda_name, lambda_package_path, lambda_role, bucket)
